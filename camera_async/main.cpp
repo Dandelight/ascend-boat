af65d02bf79deeae28c2d9afec807c67b07f40d7
dandelight <dandelight@qq.com>
2021-05-27 15:39:43 +0000

    fix: camera's fault for it's fps cannot be set to 20

--- camera_async/main.cpp
+++ camera_async/main.cpp
@@ -5,22 +5,22 @@
 #include <fcntl.h>
 #include "pthread.h"
 #include <unistd.h>
+extern "C" {
 #include "peripheral_api.h"
+}
 
 int Camera0UserFunc(const void* pdata, int size, void* param)
 {
     // pdata为指向图像内存的首地址指针，建议用户可以直接读取，但是不建议修改内容，如果需要修改，建议先拷贝到用户自定义的内存空间内再修改
-    printf("Camera0UserFunc Get frame size = %d\r\n", size);
+    printf("Camera0UserFunc Get frame size = %d ", size);
+    int checksum = 0;
+    for(int i=0; i<size; ++i) {
+        checksum += ((char*)pdata)[i];
+    }
+    printf("checksum: %0x\n", checksum);
     return 1;
 }
 
-
-int Camera1UserFunc(const void* pdata, int size, void* param)
-{
-    // pdata为指向图像内存的首地址指针，建议用户可以直接读取，但是不建议修改内容，如果需要修改，建议先拷贝到用户自定义的内存空间内再修改
-    printf("Camera1UserFunc Get frame size = %d\r\n", size);
-    return 1;
-}
 
 //进程退出时自动释放摄像头资源，不需要调用CloseCamera/CloseMic接口
 int main()
@@ -54,11 +54,6 @@ int main()
         printf("OpenCamera 0 failed %d\r\n", ret);
         return -1;
     }
-    ret = OpenCamera(1);
-    if(LIBMEDIA_STATUS_OK != ret) {
-        printf("OpenCamera 0 failed %d\r\n", ret);
-        return -1;
-    }
 
     ret = GetCameraProperty(0, CAMERA_PROP_SUPPORTED_RESOLUTION, supported_resolution);
     if(LIBMEDIA_STATUS_OK != ret) {
@@ -71,29 +66,15 @@ int main()
         i++;
     }while(supported_resolution[i].width != -1);
 
-    ret = GetCameraProperty(1, CAMERA_PROP_SUPPORTED_RESOLUTION, supported_resolution);
-    if(LIBMEDIA_STATUS_OK != ret) {
-        printf("GetCameraProperty 1 failed %d\r\n", ret);
-        return -1;
-    }
-    i = 0;
-    do{
-        printf("Camera1 supportted width = %d, height = %d\r\n", supported_resolution[i].width, supported_resolution[i].height);
-        i++;
-    }while(supported_resolution[i].width != -1);
 
     // 帧率设置
-    para = 20;
+    para = 5;
     ret = SetCameraProperty(0, CAMERA_PROP_FPS, &para);
     if(LIBMEDIA_STATUS_OK != ret) {
         printf("SetCameraProperty 0 failed %d\r\n", ret);
         return -1;
     }
-    ret = SetCameraProperty(1, CAMERA_PROP_FPS, &para);
-    if(LIBMEDIA_STATUS_OK != ret) {
-        printf("SetCameraProperty 1 failed %d\r\n", ret);
-        return -1;
-    }
+
 
     // 分辨率设置
     resolution.height = 1080;
@@ -103,21 +84,12 @@ int main()
         printf("SetCameraProperty 0 failed %d\r\n", ret);
         return -1;
     }
-    ret = SetCameraProperty(1, CAMERA_PROP_RESOLUTION, &resolution);
-    if(LIBMEDIA_STATUS_OK != ret) {
-        printf("SetCameraProperty 1 failed %d\r\n", ret);
-        return -1;
-    }
+
 
     // 注册用户回调函数
     ret = CapCamera(0, Camera0UserFunc , NULL);
     if(LIBMEDIA_STATUS_OK != ret) {
         printf("CapCamera 0 failed %d\r\n", ret);
-        return -1;
-    }
-    ret = CapCamera(1, Camera1UserFunc , NULL);
-    if(LIBMEDIA_STATUS_OK != ret) {
-        printf("CapCamera 1 failed %d\r\n", ret);
         return -1;
     }
 
@@ -127,12 +99,6 @@ int main()
     ret = CloseCamera(0);
     if(LIBMEDIA_STATUS_OK != ret) {
         printf("CloseCamera 0 failed %d\r\n", ret);
-        return -1;
-    }
-    printf("CloseCamera 1 success %d\r\n", ret);
-    ret = CloseCamera(1);
-    if(LIBMEDIA_STATUS_OK != ret) {
-        printf("CloseCamera 1 failed %d\r\n", ret);
         return -1;
     }
     printf("CloseCamera 0 success %d\r\n", ret);
